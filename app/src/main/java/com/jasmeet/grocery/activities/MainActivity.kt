fb5ec0a5e6ceacd18e0dc064e95c1f88afef17fb
saisrimanyu <saimanyu123@gmail.com>
2022-09-23 12:59:27 +0530

    final grocery

---
+++ app/src/main/java/com/jasmeet/grocery/activities/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.jasmeet.grocery.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.jasmeet.grocery.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
